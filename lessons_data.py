714f0ccd1a13ef19e1057d1438ca550a753fe8f0
Brejeshb <brejeshb.2023@scis.smu.edu.sg>
2026-03-11 17:47:34 +0800

    updated the sounds

--- lessons_data.py
+++ lessons_data.py
@@ -1,51 +1,153 @@
 # lessons_data.py
-# Your simplified lesson data with just 3 phrases
+# Complete original mixed romanization version from your app
 
 lessons_data = [
     {
         "id": 1,
-        "title": "Basic Phrases",
+        "title": "Morning Ah Ma!",
         "words": [
             {
-                "en": "Have you eaten?",
-                "hk": "食飽未？(Chia̍h-pá--bē?)",
-                "cn": "你吃了吗？",
-                "py": "nǐ chī le ma"
+                "en": "Hello",
+                "hk": "你好 (Li hó)",
+                "cn": "你好",
+                "py": "nǐ hǎo"
             },
             {
-                "en": "Drink water",
-                "hk": "喝水 (Lim tsúi)",
-                "cn": "喝水",
-                "py": "hē shuǐ"
+                "en": "Good morning",
+                "hk": "早安 (Zá an)",
+                "cn": "早安",
+                "py": "zǎo ān"
+            },
+            {
+                "en": "Aunty / Uncle",
+                "hk": "阿姨 / 阿伯 (A-yi / A-pek)",
+                "cn": "阿姨 / 叔叔",
+                "py": "ā yí / shū shū"
+            },
+            {
+                "en": "How are you today?",
+                "hk": "你现在好吗？(Li-tsuè-kīn-hó—bô?)",
+                "cn": "今天感觉怎么样？",
+                "py": "jīn tiān gǎn jué zěn me yàng"
+            },
+            {
+                "en": "I am from (country)",
+                "hk": "我是⋯⋯人 (Góa sī... kok-lâng)",
+                "cn": "我是⋯⋯人",
+                "py": "wǒ shì... rén"
             }
         ]
     },
     {
         "id": 2,
-        "title": "Full Sentence",
+        "title": "Everything Okay?",
         "words": [
             {
-                "en": "I'm going to the market to buy vegetables",
-                "hk": "我去市場買菜 (Guá khì tshī-tiûnn bé tshài)",
-                "cn": "我去市场买菜",
-                "py": "wǒ qù shì chǎng mǎi cài"
+                "en": "Do you need help?",
+                "hk": "爱帮忙吗？(Ài bāng máng bô?)",
+                "cn": "你需要帮忙吗？",
+                "py": "nǐ xū yào bāng máng ma"
+            },
+            {
+                "en": "Slowly, take your time",
+                "hk": "慢慢来 (Ban ban lai)",
+                "cn": "慢慢来",
+                "py": "màn màn lái"
+            },
+            {
+                "en": "Are you in any pain?",
+                "hk": "你有佗位痛无？(Lí ū tó-uī thiàng bô?)",
+                "cn": "你有哪里痛吗？",
+                "py": "nǐ yǒu nǎ lǐ tòng ma"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Jiak Png Liao!",
+        "words": [
+            {
+                "en": "Time to eat",
+                "hk": "食饭咯 (Jiak png lo)",
+                "cn": "吃饭了",
+                "py": "chī fàn le"
+            },
+            {
+                "en": "Is it delicious?",
+                "hk": "好食吗？(Ho jiak bo?)",
+                "cn": "好吃吗？",
+                "py": "hǎo chī ma"
+            },
+            {
+                "en": "Drink some water",
+                "hk": "喝水 (Lim zui)",
+                "cn": "喝点水",
+                "py": "hē diǎn shuǐ"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "title": "Medicine Time",
+        "words": [
+            {
+                "en": "Time for medicine",
+                "hk": "食药咯 (Jiak yok lo)",
+                "cn": "吃药时间",
+                "py": "chī yào shí jiān"
+            },
+            {
+                "en": "Swallow slowly",
+                "hk": "慢慢吞 (Ban ban thun)",
+                "cn": "慢慢吞",
+                "py": "màn màn tūn"
+            },
+            {
+                "en": "Open your mouth",
+                "hk": "哈嘴 (Hah chui)",
+                "cn": "张开嘴",
+                "py": "zhāng kāi zuǐ"
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "title": "Sleep Well",
+        "words": [
+            {
+                "en": "Time to rest",
+                "hk": "休息咯 (Hioh khun lo)",
+                "cn": "休息时间",
+                "py": "xiū xi shí jiān"
+            },
+            {
+                "en": "Sleep well",
+                "hk": "好困 (Ho khun)",
+                "cn": "好梦",
+                "py": "hǎo mèng"
+            },
+            {
+                "en": "Let's walk outside",
+                "hk": "去外面行行 (Khì guā-bīn kiânn-kiânn)",
+                "cn": "去外面走走",
+                "py": "qù wài miàn zǒu zǒu"
             }
         ]
     }
 ]
 
-# Additional standalone phrases if you want more variety
+# 2 extra longer sentences I created
 additional_phrases = [
+{
+    "en": "Joi, do you think this sounds okay?",
+    "hk": "Joi，你聽這會使嗎？(Joi, lí thiann tse ē-sái mah?)",
+    "cn": "Joi，你听这个可以吗？",
+    "py": "Joi, nǐ tīng zhè ge kě yǐ ma"
+},
     {
-        "en": "Good morning",
-        "hk": "早安 (Tsá-an)",
-        "cn": "早安",
-        "py": "zǎo ān"
-    },
-    {
-        "en": "Thank you",
-        "hk": "多謝 (To-siā)",
-        "cn": "谢谢",
-        "py": "xiè xiè"
+        "en": "Can you tell me how to get to the train station?",
+        "hk": "可以告诉我怎样去火车站吗？(Ē-sái kā guá kóng tsuánn-iūnn khì hué-tshia-tsām?)",
+        "cn": "可以告诉我怎么去火车站吗？",
+        "py": "kě yǐ gào sù wǒ zěn me qù huǒ chē zhàn ma"
     }
 ]
--- lessons_data2.py
+++ lessons_data2.py
@@ -1,62 +1,180 @@
 # lessons_data2.py
-# Tâi-lô romanization version for better TTS results
+# Complete Tâi-lô version optimized for Meta MMS-TTS-NAN model
 
 lessons_data_tailo = [
     {
         "id": 1,
-        "title": "Basic Phrases",
+        "title": "Morning Ah Ma!",
         "words": [
             {
-                "en": "Have you eaten?",
-                "hk_display": "食飽未？(Tsia̍h-pá--bē?)",  # Tâi-lô in parentheses
-                "hk_tts": "tsia̍h-pá--bē",                # Clean Tâi-lô for TTS
-                "cn": "你吃了吗？",
-                "py": "nǐ chī le ma"
+                "en": "Hello",
+                "hk_display": "你好 (Lí hó)",
+                "hk_tts": "lí hó",
+                "cn": "你好",
+                "py": "nǐ hǎo"
             },
             {
-                "en": "Drink water",
-                "hk_display": "喝水 (Lim tsuí)",
-                "hk_tts": "lim tsuí",
-                "cn": "喝水",
-                "py": "hē shuǐ"
+                "en": "Good morning",
+                "hk_display": "早安 (Tsá-an)",
+                "hk_tts": "tsá-an",
+                "cn": "早安",
+                "py": "zǎo ān"
+            },
+            {
+                "en": "Aunty / Uncle",
+                "hk_display": "阿姨 / 阿伯 (A-î / A-peh)",
+                "hk_tts": "a-î a-peh",
+                "cn": "阿姨 / 叔叔",
+                "py": "ā yí / shū shū"
+            },
+            {
+                "en": "How are you today?",
+                "hk_display": "你现在好吗？(Lí tsuè-kīn hó--bô?)",
+                "hk_tts": "lí tsuè-kīn hó--bô",
+                "cn": "今天感觉怎么样？",
+                "py": "jīn tiān gǎn jué zěn me yàng"
+            },
+            {
+                "en": "I am from (country)",
+                "hk_display": "我是⋯⋯人 (Guá sī... kok-lâng)",
+                "hk_tts": "guá sī kok-lâng",
+                "cn": "我是⋯⋯人",
+                "py": "wǒ shì... rén"
             }
         ]
     },
     {
         "id": 2,
-        "title": "Full Sentence",
+        "title": "Everything Okay?",
         "words": [
             {
-                "en": "I'm going to the market to buy vegetables",
-                "hk_display": "我去市場買菜 (Guá khì tshī-tiûnn bé tshài)",
-                "hk_tts": "guá khì tshī-tiûnn bé tshài",
-                "cn": "我去市场买菜",
-                "py": "wǒ qù shì chǎng mǎi cài"
+                "en": "Do you need help?",
+                "hk_display": "爱帮忙吗？(Ài pang-bâng bô?)",
+                "hk_tts": "ài pang-bâng bô",
+                "cn": "你需要帮忙吗？",
+                "py": "nǐ xū yào bāng máng ma"
+            },
+            {
+                "en": "Slowly, take your time",
+                "hk_display": "慢慢来 (Bān-bān lâi)",
+                "hk_tts": "bān-bān lâi",
+                "cn": "慢慢来",
+                "py": "màn màn lái"
+            },
+            {
+                "en": "Are you in any pain?",
+                "hk_display": "你有佗位痛无？(Lí ū tó-uī thiànn bô?)",
+                "hk_tts": "lí ū tó-uī thiànn bô",
+                "cn": "你有哪里痛吗？",
+                "py": "nǐ yǒu nǎ lǐ tòng ma"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Jiak Png Liao!",
+        "words": [
+            {
+                "en": "Time to eat",
+                "hk_display": "食饭咯 (Tsia̍h-pn̄g--lo)",
+                "hk_tts": "tsia̍h-pn̄g--lo",
+                "cn": "吃饭了",
+                "py": "chī fàn le"
+            },
+            {
+                "en": "Is it delicious?",
+                "hk_display": "好食吗？(Hó-tsia̍h--bô?)",
+                "hk_tts": "hó-tsia̍h--bô",
+                "cn": "好吃吗？",
+                "py": "hǎo chī ma"
+            },
+            {
+                "en": "Drink some water",
+                "hk_display": "喝水 (Lim tsuí)",
+                "hk_tts": "lim tsuí",
+                "cn": "喝点水",
+                "py": "hē diǎn shuǐ"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "title": "Medicine Time",
+        "words": [
+            {
+                "en": "Time for medicine",
+                "hk_display": "食药咯 (Tsia̍h-io̍h--lo)",
+                "hk_tts": "tsia̍h-io̍h--lo",
+                "cn": "吃药时间",
+                "py": "chī yào shí jiān"
+            },
+            {
+                "en": "Swallow slowly",
+                "hk_display": "慢慢吞 (Bān-bān thun)",
+                "hk_tts": "bān-bān thun",
+                "cn": "慢慢吞",
+                "py": "màn màn tūn"
+            },
+            {
+                "en": "Open your mouth",
+                "hk_display": "哈嘴 (Hah tshuì)",
+                "hk_tts": "hah tshuì",
+                "cn": "张开嘴",
+                "py": "zhāng kāi zuǐ"
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "title": "Sleep Well",
+        "words": [
+            {
+                "en": "Time to rest",
+                "hk_display": "休息咯 (Hioh-khun--lo)",
+                "hk_tts": "hioh-khun--lo",
+                "cn": "休息时间",
+                "py": "xiū xi shí jiān"
+            },
+            {
+                "en": "Sleep well",
+                "hk_display": "好困 (Hó khùn)",
+                "hk_tts": "hó khùn",
+                "cn": "好梦",
+                "py": "hǎo mèng"
+            },
+            {
+                "en": "Let's walk outside",
+                "hk_display": "去外面行行 (Khì guā-bīn kiânn-kiânn)",
+                "hk_tts": "khì guā-bīn kiânn-kiânn",
+                "cn": "去外面走走",
+                "py": "qù wài miàn zǒu zǒu"
             }
         ]
     }
 ]
 
+# 2 extra longer sentences in Tâi-lô
 additional_phrases_tailo = [
+{
+    "en": "Joi, do you think this sounds okay?",
+    "hk_display": "Joi，你聽這敢會使？(Joi, lí thiann tse kám ē-sái?)",
+    "hk_tts": "Joi lí thiann tse kám ē-sái",
+    "cn": "Joi，你听这个可以吗？",
+    "py": "Joi, nǐ tīng zhè ge kě yǐ ma"
+},
     {
-        "en": "Good morning",
-        "hk_display": "早安 (Tsá-an)",
-        "hk_tts": "tsá-an",
-        "cn": "早安",
-        "py": "zǎo ān"
+        "en": "Can you tell me how to get to the train station?",
+        "hk_display": "可以告诉我怎样去火车站吗？(Ē-sái kā guá kóng tsuánn-iūnn khì hué-tshia-tsām?)",
+        "hk_tts": "ē-sái kā guá kóng tsuánn-iūnn khì hué-tshia-tsām",
+        "cn": "可以告诉我怎么去火车站吗？",
+        "py": "kě yǐ gào sù wǒ zěn me qù huǒ chē zhàn ma"
     },
+    # Bonus: One more long sentence I thought would be useful
     {
-        "en": "Thank you",
-        "hk_display": "多謝 (To-siā)",
-        "hk_tts": "to-siā",
-        "cn": "谢谢",
-        "py": "xiè xiè"
-    },
-    {
-        "en": "Hello",
-        "hk_display": "你好 (Lí hó)",
-        "hk_tts": "lí hó",
-        "cn": "你好",
-        "py": "nǐ hǎo"
+        "en": "Grandma, please remember to take your medicine three times a day",
+        "hk_display": "阿妈，请记得一日三摆食药 (A-má, tshiánn kì-tit tsi̍t-ji̍t sann-pái tsia̍h-io̍h)",
+        "hk_tts": "a-má tshiánn kì-tit tsi̍t-ji̍t sann-pái tsia̍h-io̍h",
+        "cn": "阿妈，请记得一天三次吃药",
+        "py": "ā mā, qǐng jì de yī tiān sān cì chī yào"
     }
 ]
